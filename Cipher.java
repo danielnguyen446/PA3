91a9cf3b94de183111bb9a823a5ab876da2ede4d
danielnguyen446 <77251383+danielnguyen446@users.noreply.github.com>
2021-01-26 22:15:58 -0800

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
  * The second part encodes and decodes a single character by performing a caesar shift.
  * The third part performs the Vigenere cipher on an entire string and uses a string as the key.
  */
-public class Main
+public class Cipher
 {
     
     /*Part 1:These methods check validity of the input*/
